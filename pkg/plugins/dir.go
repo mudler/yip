e7fba7a22570be96a82f496f57eee69bdb32c2f2
David Cassany <dcassany@suse.com>
2021-04-23 13:20:23 +0200

    Create parent directories if missing for dir and files plugins
    This commit adds the logic to create parent directories structure of a
    provided path in dir or files plugin does not exist.
    
    For dir plugin if the path already exists and it is a directory it
    applies permissions and ownership only instead of failing.
    
    Signed-off-by: David Cassany <dcassany@suse.com>

--- pkg/plugins/dir.go
+++ pkg/plugins/dir.go
@@ -1,7 +1,9 @@
 package plugins
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/mudler/yip/pkg/schema"
@@ -12,7 +14,7 @@ import (
 func EnsureDirectories(s schema.Stage, fs vfs.FS, console Console) error {
 	var errs error
 	for _, dir := range s.Directories {
-		if err := writeDirectory(dir, fs); err != nil {
+		if err := writePath(dir, fs, true); err != nil {
 			log.Error(err.Error())
 			errs = multierror.Append(errs, err)
 			continue
@@ -29,4 +31,35 @@ func writeDirectory(dir schema.Directory, fs vfs.FS) error {
 	}
 
 	return fs.Chown(dir.Path, dir.Owner, dir.Group)
+}
+
+func writePath(dir schema.Directory, fs vfs.FS, topLevel bool) error {
+	inf, err := fs.Stat(dir.Path)
+	if err == nil && inf.IsDir() && topLevel {
+		// The path already exists, apply permissions and ownership only
+		err = fs.Chmod(dir.Path, os.FileMode(dir.Permissions))
+		if err != nil {
+			return err
+		}
+		return fs.Chown(dir.Path, dir.Owner, dir.Group)
+	} else if err == nil && !inf.IsDir() {
+		return fmt.Errorf("Error, '%s' already exists and it is not a directory", dir.Path)
+	} else if err == nil {
+		return nil
+	} else {
+		parentDir := filepath.Dir(dir.Path)
+		_, err = fs.Stat(parentDir)
+		if parentDir == "/" || parentDir == "." || err == nil {
+			//There is no parent dir or it already exists
+			return writeDirectory(dir, fs)
+		} else {
+			//Parent dir needs to be created
+			pDir := schema.Directory{parentDir, dir.Permissions, dir.Owner, dir.Group}
+			err = writePath(pDir, fs, false)
+			if err != nil {
+				return err
+			}
+			return writeDirectory(dir, fs)
+		}
+	}
 }
