9d04a4d0c7d79ff956878973ac62e81f3da828a8
Itxaka <itxaka@kairos.io>
2023-09-11 21:21:57 +0200

    Remove force name for cloud init configs
    Otherwise, in files which have no naming for example:
    
    ```
    stages:
      default:
        - commands:
          - echo "01"
    ```
    
    The name of the whole thing will be autogenerated with a fixed string
    and it will collide if there is more than one op across different files.
    
    Thus it would somehow miss the files in between the first and the last.
    So if you had 5 files it would ignore the 2,3,4 in teh middle and only
    run the first and last.
    
    This is due the construction fo the op name which would be like:
    `config.Name.Op_number`
    
    Probably because this files are coming from the cloud init loader, the
    config name will never change, thus generating ops with the final name
    for all of them:
    
    `Cloud init.0`
    
    This patch removes the forced set of the config name to `Cloud init` so
    it can be autogenerated based on the file name the op is coming from
    thus avoiding any collision in names.
    
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- pkg/executor/default_test.go
+++ pkg/executor/default_test.go
@@ -631,19 +631,13 @@ stages:
 
 			Expect(string(b)).Should(Equal("rootfs.before\nsecond.rootfs.before\nrootfs\n2\nsecond.rootfs\nsecond.2\ninitramfs\nsecond.initramfs\n"), string(b))
 		})
-		It("has multiple instructions in different files v2", Focus, func() {
+		It("same instructions in different cloud-config files", func() {
 			buf := bytes.Buffer{}
 			l := logrus.New()
 			l.SetOutput(&buf)
 			l.SetLevel(logrus.DebugLevel)
 			def := NewExecutor(WithLogger(l))
-			testConsole := consoletests.New()
-			cmds := []consoletests.CmdMock{
-				{Cmd: "echo \"01\"", Output: "01"},
-				{Cmd: "echo \"02\"", Output: "02"},
-				{Cmd: "echo \"03\"", Output: "03"},
-			}
-			testConsole.AddCmds(cmds)
+			testConsole := console.NewStandardConsole()
 
 			temp, err := os.MkdirTemp("", "")
 			Expect(err).ToNot(HaveOccurred())
@@ -655,13 +649,13 @@ stages:
 			err = os.WriteFile(filepath.Join(temp, "03_test.yaml"), []byte("#cloud-config\nstages:\n  default:\n    - commands:\n      - echo \"03\"\n"), os.ModePerm)
 			Expect(err).ToNot(HaveOccurred())
 
-			err = def.Run("initramfs", vfs.OSFS, testConsole, temp)
-			g, _ := def.Graph("initramfs", vfs.OSFS, testConsole, temp)
+			err = def.Run("default", vfs.OSFS, testConsole, temp)
 			Expect(err).Should(BeNil())
 			Expect(buf.String()).To(ContainSubstring(fmt.Sprintf("Reading '%s'", filepath.Join(temp, "01_test.yaml"))), buf.String())
 			Expect(buf.String()).To(ContainSubstring(fmt.Sprintf("Reading '%s'", filepath.Join(temp, "02_test.yaml"))), buf.String())
 			Expect(buf.String()).To(ContainSubstring(fmt.Sprintf("Reading '%s'", filepath.Join(temp, "03_test.yaml"))), buf.String())
 			// 3 commands + init in the graph
+			g, _ := def.Graph("default", vfs.OSFS, testConsole, temp)
 			Expect(len(g)).To(Equal(4), litter.Sdump(g))
 
 		})
