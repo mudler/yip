1315f5147a84c371fd26042daec39fe834b236b8
Dimitris Karakasilis <dimitris@karakasilis.me>
2026-04-22 10:46:52 +0300

    Fix go-diskfs after version bump
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- pkg/plugins/layout.go
+++ pkg/plugins/layout.go
@@ -430,6 +430,7 @@ func (dev *Disk) AddPartitions(parts []schema.Partition, l logger.Interface, con
 		}
 
 		part := &gpt.Partition{
+			Index:      len(gptTable.Partitions) + 1,
 			Start:      start,
 			End:        end,
 			Name:       p.PLabel,
